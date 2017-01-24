2cbf74ea793744ed3944384d0465ee2a7d419993
Kathy Tran <ktran77@gatech.edu>
2017-01-24 00:19:10 -0500

    Kathy implemented method in Person4.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,14 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      char value;
+      String toReturn = "";
+      for(int i = 0; i < input.length(); i++) {
+        value = input.charAt(i);
+        System.out.println(value + "" );
+        toReturn = toReturn + String.valueOf((char) (value+1));
+      }
+      return toReturn;
     }
     
     /**
